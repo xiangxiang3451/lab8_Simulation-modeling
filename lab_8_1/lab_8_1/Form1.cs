1c618db79457eca1d66cc5db3c5fd888bfe2c969
Chang <xiangxiang3451@gmail.com>
2024-04-29 23:31:52 +0700

    lab8_1 finished

---
+++ lab_8_1/lab_8_1/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_8_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random random = new Random();
+            ans.Text = random.Next(10) > 5 ? "yes" : "no";
+        }
+    }
+}
