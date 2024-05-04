0dc7b3da4137d69a60892ab8fb42486f78076f9b
Chang <xiangxiang3451@gmail.com>
2024-05-05 00:17:52 +0700

    lab8_2

---
+++ lab_8_2/lab_8_2/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace lab_8_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ans = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("宋体", 20F);
+            this.textBox1.Location = new System.Drawing.Point(2, 2);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(479, 53);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "Magic 8-Ball";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Font = new System.Drawing.Font("宋体", 26F);
+            this.textBox2.Location = new System.Drawing.Point(239, 140);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(651, 67);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "Will I become rich?";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("宋体", 20F);
+            this.button1.Location = new System.Drawing.Point(412, 303);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(175, 79);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Guess";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("宋体", 20F);
+            this.label1.Location = new System.Drawing.Point(157, 489);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(157, 40);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Answer:";
+            // 
+            // ans
+            // 
+            this.ans.Font = new System.Drawing.Font("宋体", 10F);
+            this.ans.Location = new System.Drawing.Point(320, 503);
+            this.ans.Name = "ans";
+            this.ans.Size = new System.Drawing.Size(799, 30);
+            this.ans.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1194, 668);
+            this.Controls.Add(this.ans);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox ans;
+    }
+}
+
---
+++ lab_8_2/lab_8_2/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace lab_8_2
+{
+    public partial class Form1 : Form
+    {
+
+        private List<string> answers;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+            // Probability of events
+            double[] probabilities = { 0.1, 0.2, 0.3, 0.4 };
+
+            // Initialize the random number generator
+            Random random = new Random();
+
+            // Get event number
+            int eventIndex = GetEventIndex(probabilities, random);
+
+            switch (eventIndex)
+            {
+                case 0:
+                    ans.Text = ("Да");
+                    break;
+                case 1:
+                    ans.Text = ("Нет");
+                    break;
+                case 2:
+                    ans.Text = ("Возможно");
+                    break;
+                case 3:
+                    ans.Text = ("Не могу сейчас предсказать");
+                    break;
+                default:
+                    ans.Text = ("Сосредоточьтесь и спросите снова");
+                    break;
+            }
+        }
+        private int GetEventIndex(double[] probabilities, Random random)
+        {
+            // Calculate the total probability,the sum of the probabilities of all events
+            double totalProbability = 0;
+            foreach (var probability in probabilities)
+            {
+                totalProbability += probability;
+            }
+
+            // Randomly generate a probability value
+            double randomValue = random.NextDouble() * totalProbability;
+
+            // Calculate event number
+            int eventIndex = 0;
+            double cumulativeProbability = 0;
+            foreach (var probability in probabilities)
+            {
+                cumulativeProbability += probability;
+                if (randomValue <= cumulativeProbability)
+                {
+                    break;
+                }
+                eventIndex++;
+            }
+
+            return eventIndex;
+        }
+    }
+}
